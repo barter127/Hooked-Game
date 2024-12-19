536c07d882eef6a21d85148c58dc75774d90f52f
Heather Carter <heathercarter2006@outlook.com>
2024-12-19 21:58:11 +0000

    Knife Return
    Switch to force movement

--- Assets/Scripts/PlayerShootLogic.cs
+++ Assets/Scripts/PlayerShootLogic.cs
@@ -3,16 +3,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerShootLogic : MonoBehaviour
 {
-    [SerializeField] GameObject m_projectile;
+    // Knife Prefab.
+    [SerializeField] GameObject m_knifeObject;
+    // Player Transform.
     [SerializeField] Transform m_playerTrans;
 
     // Point projectile spawns from.
     [SerializeField] Transform m_firePointTrans;
 
+    [Header ("Knife Return")]
+
+    // Speed knife returns to player.
+    [SerializeField] float m_returnSpeed;
+    // Distance needed to destroy knife.
+    [SerializeField] float m_returnMagnitude;
+
     bool m_hasFired;
+    bool m_isReturning;
     GameObject m_knifeReference;
 
     private InputAction m_attackAction;
+
+
 
     #region Handle Input Sys
 
@@ -36,6 +48,12 @@ public class PlayerShootLogic : MonoBehaviour
     void Update()
     {
         RotateToMouse();
+
+
+        if (m_isReturning)
+        {
+            ReturnKnife();
+        }
     }
 
     // Rotate cursor around player and point to current mouse position.
@@ -61,21 +79,52 @@ public class PlayerShootLogic : MonoBehaviour
         }
     }
 
-    // On click spawn knife and fire in mouse direction.
+    // Attack Button Pressed.
     void Attack(InputAction.CallbackContext context)
     {
+        if (!m_hasFired)
+        {
+            FireKnife();
+        }
+        else
+        {
+            m_isReturning = true;
+        }
+    }
+
+    // On click spawn knife and fire in mouse direction.
+    void FireKnife()
+    {
+        m_hasFired = true;
+
         // Get direction bullet needs to travel.
         Vector3 bulletDir = (m_firePointTrans.position - m_playerTrans.position).normalized;
-
-        Debug.Log(bulletDir);
 
         // Find rotation for spawned bullet.
         float radvalue = Mathf.Atan2(bulletDir.y, bulletDir.x);
         float angle = radvalue * Mathf.Rad2Deg;
 
         // Fire projectile in direction. Rotated to face direction.
-        m_knifeReference = Instantiate(m_projectile, m_firePointTrans.position, Quaternion.identity);
+        m_knifeReference = Instantiate(m_knifeObject, m_firePointTrans.position, Quaternion.identity);
         m_knifeReference.transform.Rotate(Vector3.forward, angle - 90); // 90 is an arbitrary value to fix offset.
         m_knifeReference.GetComponent<Rigidbody2D>().AddForce(bulletDir * 10, ForceMode2D.Impulse);
+    }
+
+    // Move towards player. Delete at destination.
+    void ReturnKnife()
+    {
+        // Knife hit destination
+
+        // DEFINITLY SHORTEN THIS.
+        if (Mathf.Abs(m_knifeReference.transform.position.magnitude - m_playerTrans.position.magnitude) <= m_returnMagnitude)
+        {
+            Destroy(m_knifeReference.gameObject);
+            m_knifeReference = null;
+
+            m_isReturning = false;
+            m_hasFired = false;
+        }
+
+        m_knifeReference.transform.position = Vector3.MoveTowards(m_knifeReference.transform.position, m_playerTrans.position, m_returnSpeed * Time.deltaTime);
     }
 }
