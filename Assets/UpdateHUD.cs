658ce69169980b9c695273d8cea6848376eab394
Heather Carter <heathercarter2006@outlook.com>
2025-01-07 10:49:44 +0000

    Sprite References

--- Assets/UpdateHUD.cs
+++ Assets/UpdateHUD.cs
@@ -7,8 +7,17 @@ public class UpdateHUD : MonoBehaviour
 {
     // Holds public methods to update UI
 
+    [Header ("Health Component Reference")]
     public List<Image> m_UIHeartImages = new List<Image>();
-    int m_currentUIHealth = 16;
+    int m_currentUIHealth = 8;
+    int m_currentMaxUIHealth = 8;
+
+    [Header("Health Images")]
+    [SerializeField] Sprite m_fullHeartImg;
+    [SerializeField] Sprite m_halfHeartImg;
+    [SerializeField] Sprite m_emptyHeartImg;
+
+    [SerializeField] Sprite m_nullHeartImg; // Indicate health that can be gained.
 
     private void Start()
     {
@@ -29,12 +38,16 @@ public class UpdateHUD : MonoBehaviour
     // Set Health UI based on parameter.
     public void SetHealthUI(int newHealth) // Floats don't match my game design ethos. Just use ints.
     {
+        for (int i = 0; i < m_UIHeartImages.Count; i++)
+        {
+
+        }
+
         //m_healthText.text = newHealth.ToString();
     }
 
     // Increase or decrease Health UI based on parameter.
     public void IncreaseHealthUI(int healthChange)
     {
-        m_UIHeartImages
     }
 }
