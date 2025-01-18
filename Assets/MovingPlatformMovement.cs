c5928e325f737c6b7a21e265f4dcf5510cc6ff46
barter127 <heathercarter2006@outlook.com>
2025-01-18 19:46:19 +0000

    Moving Platform

---
+++ Assets/MovingPlatformMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class MovingPlatformMovement : MonoBehaviour
+{
+    [Header("Start and End")]
+    // Vector3 so matches transform.
+    Vector3 m_startPos;
+    Vector3 m_endPos;
+    [SerializeField] Transform m_endPosTransform;
+
+    [Header("Rate of Movement")]
+    [SerializeField] AnimationCurve m_movementCurve;
+    [SerializeField] float m_timeToMove;
+    float m_elapsedTime;
+    bool m_movingToEnd;
+
+
+    void Start()
+    {
+        // Get vector values.
+        m_startPos = transform.position;
+        m_endPos = m_endPosTransform.position;
+    }
+
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    void MovePlatform()
+    {
+        // Get percentage complete.
+        float percentComplete = m_movementCurve.Evaluate(m_elapsedTime / m_timeToMove);
+
+        // Reset timer.
+        if (percentComplete >= 1)
+        {
+            m_elapsedTime = 0;
+            m_movingToEnd = !m_movingToEnd;
+        }
+
+
+        // Lerp position to target.
+        if (m_movingToEnd) // Start to End.
+        {
+            transform.position = Vector3.Lerp(m_startPos, m_endPos, percentComplete);
+        }
+        else // End to Start.
+        {
+            transform.position = Vector3.Lerp(m_endPos, m_startPos, percentComplete);
+        }
+
+        m_elapsedTime += Time.deltaTime;
+    }
+}
