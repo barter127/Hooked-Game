661740ac2baf1b1a8c6273709f3a5657d0689f65
barter127 <heathercarter2006@outlook.com>
2025-01-23 19:41:09 +0000

    Music Settings Save between scenes

--- Assets/Options.cs
+++ Assets/Options.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
+    public float m_musicVolume { get; private set; }
+    public float m_sfxVolume { get; private set; }
+
     // Audio Player.
-    AudioSource m_musicSource;
+    public AudioSource m_musicSource;
 
     // Slider references
     [SerializeField] Slider m_musicSlider;
@@ -16,21 +20,38 @@ public class Options : MonoBehaviour
         m_musicSlider.onValueChanged.AddListener(OnMusicSliderValueChanged);
         m_sfxSlider.onValueChanged.AddListener(OnSFXSliderValueChanged);
 
-        //
+        // Set initial volume values.
+        m_musicVolume = m_musicSlider.value;
+        m_sfxVolume = m_sfxSlider.value;
+
+        // Find the music player and set the volume again
         GameObject musicPlayer = GameObject.FindWithTag("Music Player");
         m_musicSource = musicPlayer.GetComponent<AudioSource>();
+        m_musicSource.volume = m_musicVolume;
 
-        // Manually trigger event.
-        OnMusicSliderValueChanged(m_musicSlider.value);
+        // Subscribe to the activeSceneChanged event
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+    }
+
+    void ChangedActiveScene(Scene current, Scene next)
+    {
+        Debug.Log("Hi");
+
+        // Find the music player and set the volume again
+        GameObject musicPlayer = GameObject.FindWithTag("Music Player");
+        m_musicSource = musicPlayer.GetComponent<AudioSource>();
+        m_musicSource.volume = m_musicVolume;
     }
 
     void OnMusicSliderValueChanged(float value)
     {
-        m_musicSource.volume = value;
+        m_musicVolume = value;
+        m_musicSource.volume = m_musicVolume;
     }
 
     void OnSFXSliderValueChanged(float value)
     {
-        Debug.Log("I am not functional");
+        Debug.Log("SFX Slider Value Changed: " + value);
+        // Add your custom logic here for the SFX slider
     }
 }
