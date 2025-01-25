856e3b151b1fc8abb2e37205987daab752faa405
Heather Carter <heathercarter2006@outlook.com>
2025-01-25 02:38:20 +0000

    Save and Load system

---
+++ Assets/JsonReadWriteSystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.IO;
+
+public class JsonReadWriteSystem : MonoBehaviour
+{
+    public static void SaveDeathStatisticDataToJson(int totalDeaths, int totalKills)
+    {
+        PlayerSaveData data = new PlayerSaveData();
+        data.m_totalDeaths = totalDeaths;
+        data.m_totalKills = totalKills;
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(Application.dataPath + "/PlayerSaveDataFile.json", json);
+    }
+
+    public static PlayerSaveData LoadDeathStatisticDataFromJson()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/PlayerSaveDataFile.json");
+        PlayerSaveData data = JsonUtility.FromJson<PlayerSaveData>(json);
+
+
+    }
+}
---
+++ Assets/PlayerSaveData.cs
@@ -0,0 +1,13 @@
+// Holds player statistics and settings data
+// Will be converted to Json.
+
+public class PlayerSaveData
+{
+    // Statistics
+    public int m_totalDeaths;
+    public int m_totalKills;
+
+    // Settings
+    public float m_musicVolume;
+    public float m_sfxVolume;
+}
