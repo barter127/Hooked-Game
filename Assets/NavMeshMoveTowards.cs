9c8f851b6055631ab06352371d9cfa0689296013
Heather Carter <heathercarter2006@outlook.com>
2025-01-26 00:17:39 +0000

    Flip Gaper

--- Assets/NavMeshMoveTowards.cs
+++ Assets/NavMeshMoveTowards.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent (typeof(SpriteRenderer))]
+[RequireComponent (typeof(NavMeshAgent))]
 public class NavMeshMoveTowards : MonoBehaviour
 {
     [SerializeField] Transform m_targetTransform;
     NavMeshAgent m_agent;
+    SpriteRenderer m_spriteRenderer;
 
     // Time spent paused after getting in stopping range.
     [SerializeField] float pauseLength;
     public bool m_isChasing = true; // For animator.
 
+    bool m_isFacingRight = true;
+
     void Start()
     {
         m_agent = GetComponent<NavMeshAgent>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
 
         m_isChasing = true;
     }
@@ -22,11 +28,13 @@ public class NavMeshMoveTowards : MonoBehaviour
     {
         m_agent.SetDestination(m_targetTransform.position);
 
+        float relativeXPos = m_targetTransform.position.x - transform.position.x;
+        CheckDirectionToFace(relativeXPos < 0);
+
         // In stopping range.
         float distance = Vector2.Distance(transform.position, m_targetTransform.position);
         if (m_agent.stoppingDistance > distance)
         {
-            Debug.Log("HI");
             PauseAIMovement(pauseLength);
         }
     }
@@ -46,5 +54,19 @@ public class NavMeshMoveTowards : MonoBehaviour
 
         m_isChasing = true;
         m_agent.isStopped = false;
+    }
+
+    void Turn()
+    {
+        m_isFacingRight = !m_isFacingRight;
+        m_spriteRenderer.flipX = m_isFacingRight;
+    }
+
+    void CheckDirectionToFace(bool isMovingRight)
+    {
+        if (isMovingRight != m_isFacingRight)
+        {
+            Turn();
+        }
     }
 }
