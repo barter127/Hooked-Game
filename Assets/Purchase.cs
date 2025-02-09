d4890436cdfc0d48108ec29774af9e2198e3f78e
Heather Carter <heathercarter2006@outlook.com>
2025-02-09 18:49:53 +0000

    Purchaseable value will always be correct

--- Assets/Purchase.cs
+++ Assets/Purchase.cs
@@ -1,14 +1,24 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Purchase : MonoBehaviour
 {
+    [SerializeField] TextMeshPro m_moneyText;
+
+    [Header ("Stats")]
     [SerializeField] int m_price;
     [SerializeField] int m_damage;
     [SerializeField] int m_speed;
 
     public UnityEvent m_itemBought;
+
+    void Start()
+    {
+        // Ensure Text value is correct.
+        m_moneyText.text = "£" + m_price.ToString();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
