871980966c51e6f9aa8846e277fe085638e70b9f
Heather Carter <heathercarter2006@outlook.com>
2025-02-09 09:08:18 +0000

    Double Shot

--- Assets/Scripts/EnemyFire.cs
+++ Assets/Scripts/EnemyFire.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using UnityEngine.LightTransport;
 using UnityEngine.Rendering;
 
 public class EnemyFire : MonoBehaviour
 {
     StateMachine m_stateMachine;
 
-    [Header ("Firing Vars")]
-    [SerializeField] GameObject bullet;
     [SerializeField] Transform m_target;
 
+    [Header ("Firing Vars")]
+    [SerializeField] GameObject bullet;
+
     [SerializeField] float m_fireTimerLength;
-    [SerializeField] float m_pauseLength;
     float m_fireTimer;
+
+    [SerializeField] float m_angleBetweenBullets;
+
+    [SerializeField] float m_pauseLength;
 
     [SerializeField] Transform m_firePoint;
     StraightToPathfinding m_movement;
@@ -34,12 +39,12 @@ public class EnemyFire : MonoBehaviour
             {
                 m_movement.PauseAIMovement(m_pauseLength);
 
-                FireBullet();
+                FireDoublebullet();
                 m_fireTimer = m_fireTimerLength;
             }
             else if (m_stateMachine.m_currentState == StateMachine.AIState.Attached)
             {
-                FireBullet();
+                Attack();
 
                 // Fires twice as fast.
                 m_fireTimer = m_fireTimerLength / 2;
@@ -49,11 +54,45 @@ public class EnemyFire : MonoBehaviour
         else m_fireTimer -= Time.deltaTime;
     }
 
-    void FireBullet()
+    void Attack()
     {
+        // Randomise Firing Type.
+        bool doubleShot = Random.Range(0, 2) == 1;
+
         Vector3 bulletDir = (m_target.position - transform.position).normalized;
 
         GameObject bulletReference = Instantiate(bullet, m_firePoint.position, Quaternion.identity);
         bulletReference.GetComponent<BulletMovement>().SetDirection(bulletDir);
     }
+
+    void FireSinglebullet()
+    {
+        // Get shooting Direction
+        Vector3 bulletDir = (m_target.position - transform.position).normalized;
+
+        // Spawn bullet and set its direction.
+        GameObject bulletReference = Instantiate(bullet, m_firePoint.position, Quaternion.identity);
+        bulletReference.GetComponent<BulletMovement>().SetDirection(bulletDir);
+    }
+
+    void FireDoublebullet()
+    {
+        // Get shooting direction.
+        Vector3 aimingDir = (m_target.position - transform.position).normalized;
+
+        // Find angle of aiming directions vector.
+        float attackAngle = Mathf.Atan2(aimingDir.y, aimingDir.x);
+
+        // This could be more customiseable but focusing on other AI.
+        for (int i = -1; i < 2; i += 2)
+        {
+            // Get Direction to shoot each bullet (first iter negative, 2nd positive)
+            Vector3 bulletDir = new Vector3(Mathf.Cos(attackAngle + Mathf.Deg2Rad * m_angleBetweenBullets * i), Mathf.Sin(attackAngle + Mathf.Deg2Rad * m_angleBetweenBullets * i), 0);
+
+            // Spawn bullet and set its direction.
+            GameObject bulletReference = Instantiate(bullet, m_firePoint.position, Quaternion.identity);
+            bulletReference.GetComponent<BulletMovement>().SetDirection(bulletDir);
+        }
+    }
+
 }
